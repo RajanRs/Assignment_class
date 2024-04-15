5d1c7640815308ceeafa663e50ef4d4cddd4ce14
RajanRs <rajonchandra013@gmail.com>
2024-04-16 00:04:45 +0600

    second commit

--- Bank_account_app/Bank.js
+++ Bank_account_app/Bank.js
@@ -1,12 +1,41 @@
+// Define the BankAccount class
 class BankAccount {
-    displayAccountInfo(){
-        console.log(`Account Number: ${this.accountNumber}`);
-        console.log(`Owner Name: ${this.ownerName}`);
-        console.log(`Balance: $${this.balance}`);
+    constructor(accountNumber, ownerName, balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+        console.log(`$${amount} deposited successfully.`);
+    }
+
+    withdraw(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            console.log(`$${amount} withdrawn successfully.`);
+        } else {
+            console.log("Insufficient funds.");
+        }
+    }
+
+    getBalance() {
+        return this.balance;
+    }
+
+    displayAccountInfo() {
+        console.log("Account Number:", this.accountNumber);
+        console.log("Owner Name:", this.ownerName);
+        console.log("Balance: $" + this.balance);
     }
 }
-const account1= new BankAccount(1001, "Jone Doe", 1000);
-const account2= new BankAccount(1002, "Jane Smith", 500);
-console.log("Initial Account Information:");
+const account1 = new BankAccount(1001, "John Doe", 1000);
+const account2 = new BankAccount(1002, "Jane Smith", 2000);
+account1.deposit(500);
+account2.withdraw(1000);
+console.log("Deposit and Withdrawal :")
+console.log("Initial Account Information :");
 account1.displayAccountInfo();
+console.log("Updated Account Information:");
 account2.displayAccountInfo();
